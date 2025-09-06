c1783f2a3b7d96e824b4bb522681726b279e4808
Lehonti Ramos <17771375+Lehonti@users.noreply.github.com>
2025-09-07 01:50:50 +0200

    Scaffolding. Implementation pending

---
+++ tests/Pinta.Core.Tests/BlendOpTests/BlendOpTests.Ops.ColorDodge.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Pinta.Core.Tests;
+
+partial class BlendOpTests
+{
+	private static readonly UserBlendOps.ColorDodgeBlendOp color_dodge_op;
+
+	private static IEnumerable<TestCaseData> CreateColorDodgeIOCases (UserBlendOps.ColorDodgeBlendOp colorDodgeOp)
+	{
+		throw new NotImplementedException ();
+	}
+}
